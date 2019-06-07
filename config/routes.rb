6573f54663c3d48c8af4ff22719bac7579243c4f
Ivan Vergés <ivan@platoniq.net>
2019-06-07 15:19:38 +0200

    Goto to consultation if active

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,19 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  devise_for :users,
+             class_name: "Decidim::User",
+             module: :devise,
+             router_name: :decidim,
+             controllers: {
+               invitations: "decidim/devise/invitations",
+               sessions: "decidim/devise/sessions",
+               confirmations: "decidim/devise/confirmations",
+               registrations: "decidim/devise/registrations",
+               passwords: "passwords",
+               omniauth_callbacks: "decidim/devise/omniauth_registrations"
+             }
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
